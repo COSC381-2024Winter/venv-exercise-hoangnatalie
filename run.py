907c32be7a6348d6bd5f6950201b8c76a42623ea
Natalie Hoang <120707133+hoangnatalie@users.noreply.github.com>
2024-03-13 03:34:23 +0000

    Found 3 dependencies from pypi.org Used those dependencies in the program.
    issue #1

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 #This is my python program
 
+#Emoji shows thumbs up
 import sys
 from emoji import emojize
 
 print(emojize(":thumbs_up:"))
+
+#Displays a cat picture ^.^
+from cat_picture.cat import pic
+pic()
+
+#Get a random joke
+import pyjokes
+print(pyjokes.get_joke())
